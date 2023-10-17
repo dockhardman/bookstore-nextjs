7e2600e4030226a68394ddc04dd79cf25a7ee801
Allen Chou <f1470891079@gmail.com>
2023-10-17 19:50:33 +0800

    Add dashboard link

--- app/page.js
+++ app/page.js
@@ -1,5 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div>
+      <h1>Hello, Next.js!</h1>
+      <Link href="/dashboard">Dashboard</Link>
+    </div>
+  );
 }
